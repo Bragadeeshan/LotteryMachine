6a6983a563409afc03333735e48af1e03cad0469
Bragadeeshan <68198134+Bragadeeshan@users.noreply.github.com>
2021-12-01 17:45:42 +0530

    Added the part where Adding gift or spin the wheel

--- Lottery/Lottery.py
+++ Lottery/Lottery.py
@@ -1,17 +1,41 @@
 import random
 import csv
 
-with open('Gifts.csv', 'w', newline='') as file:
+with open('Gifts.csv','w+', newline='') as file:
     writer = csv.writer(file)
     writer.writerow(["Gift", "Amount", "Weight"])
     noOfGifts=int(input("Please enter the number of Gifts !! "))
     for i in range(noOfGifts) :
         print(" Gift :",i+1)
-        gift = input("Enter Gift Name ")
-        amount = input("Enter the Number of Gift")
-        weight = input("Weight of the Gift")
+        gift = input("Enter Gift Name : ")
+        amount = int(input("Enter the Number of Gift : "))
+        weight = int(input("Weight of the Gift : "))
         writer.writerow([gift, amount, weight])
-    
+    while (1) :
+        Decision = input("Add Gift(A) or Spin the wheel(S)  \n Choose A or S ")
 
-    
-    
+        if Decision == "A" :
+            gift = input("Enter Gift Name ")
+            amount = input("Enter the Number of Gift : ")
+            weight = input("Weight of the Gift : ")
+            writer.writerow([gift, amount, weight])
+
+        elif Decision == "S" :
+            rand = random.Random(0,100)
+            if rand > 80 :
+                print("No Luck !!")
+                break
+            elif rand >70 :
+                print("try again")
+            
+            
+            total = 0
+            for row in csv.reader(file):
+                for col in row[2]:
+                    total += int(col)
+
+        elif Decision == "E" :
+            break
+
+        else :
+            print("Input is Wrong ! Choose Between A/ S / E")
