9d3b88a60a0a0417ae22ab1e463f959cdf54db6b
Bragadeeshan <68198134+Bragadeeshan@users.noreply.github.com>
2021-12-02 00:17:03 +0530

    Error Corrections

--- Lottery/Lottery.py
+++ Lottery/Lottery.py
@@ -9,65 +9,71 @@ print(df)
 giftlist = ()
 
 while (1) :
-        Decision = input("Add Gift(A) or Spin the wheel(S)  \n Choose A or S : ")
-        flag = 0
-        if Decision == "A" :
+        Decision = input("Add Gift(A) or Spin the wheel(S)  \n Choose A or S : ") #decision between adding and Spinning the wheel
+        flag = 0 #flag to check whether a gift is already there or not
+        if Decision == "A" : #if the user wanted to add an item
             gift = input("Enter Gift Name ")
             amount = int(input("Enter the Number of Gift : "))
             weight = input("Weight of the Gift : ")
-            for i in range(len(df)) :
+            for i in range(len(df)) : #loop to check through the csv
                 if df['Gift'][i] == gift :
-                    df.loc[i,'Amount'] -= amount
-                    df.loc[i,'Weight'] = weight
+                    df.loc[i,'Amount'] = df.loc[i,'Amount'] + amount #if the gift is already there adding the amount
+                    df.loc[i,'Weight'] = weight #changing the weight if changed
                     flag=1
                 
 
-            if flag==0 :
-                df.loc[i+1,'Gift'] = gift
+            if flag==0 : #if the item is not in the csv
+                df.loc[i+1,'Gift'] = gift 
                 df.loc[i+1,'Amount'] = amount
-                df.loc[i+1,'Weight'] = weight
+                df.loc[i+1,'Weight'] = weight #adding the item to the list
 
-        elif Decision == "S" :
-            rand = random.randint(0,100)
+        elif Decision == "S" : #if Spinning the wheel got selected by the user
+            rand = random.randint(0,100) #selecting a random number
 
-            if rand > 95 :
+            if rand > 95 : # if the random number is above 95 the player loses
                 print("No Luck !!")
                 break
-            elif rand > 90 :
+            elif rand > 90 : #if the number is between 95 and 90 player can try again
                 print("try again")
 
             else :
                 prob = 0
                 total = 0
                 x=0
-                print ("Check point 1")
-                for col in df["Weight"]:
+                #print ("Check point 1")
+                for col in df["Weight"]: #calculating the total weight
                     total += int(col)
                         
-                print(total)
+                #print(total)
                
                 for col in df["Weight"]:
                         x = int(col)
-                        prob = x/total
-                        k.append(prob)
+                        prob = x/total #calculating the probability
+                        k.append(prob) #appending it to a lis
 
                 giftlist = list(df["Gift"])
-                giftDict = dict(zip(k,giftlist))
+                giftDict = dict(zip(k,giftlist)) #creating a list to hold the gift and the probability
                 
                 #giftDict = dict(giftDict)
-                rand = random.randint(0,100)/100
+                rand = random.randint(0,100)/100 #getting a random number between 100
+                gift = 0
                 for i in sorted(giftDict):
                     if rand < i :
                         print("Congratulations You have won ", giftDict[i])
                         gift = giftDict[i]
                         break
-                
+                if(gift == 0) :
+                    print("Congratulations You have won ", giftDict[i])
+                    gift = giftDict[i]    
+               
                 for i in range(len(df)) :
                     if df['Gift'][i] == gift :
 
-                         df.loc[i,'Amount'] -= 1
+                         df.loc[i,'Amount'] = df.loc[i,'Amount'] - 1 #deducting the gift from the dataframe
+                         if  df.loc[i,'Amount'] == 0 :
+                             data = data.drop(index = gift, axis=0) #if the number of gifts go to zero drop it 
 
-            #break
+            break
 
         else :
             print("Input is Wrong ! Choose Between A / S ")
