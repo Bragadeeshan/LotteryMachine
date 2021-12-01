3c8975f8b7178129433262c1600a85d05b305ec6
Bragadeeshan <68198134+Bragadeeshan@users.noreply.github.com>
2021-12-01 23:32:40 +0530

    Created a Dictionary to hold the probability
    Created a dictionary to hold the probabilities of the gifts now we can edit the csv , the gift can be delivered

--- Lottery/Lottery.py
+++ Lottery/Lottery.py
@@ -2,26 +2,30 @@ import random
 import csv
 import pandas as pd
 
-
+giftDict = {}
+k=[]
 df = pd.read_csv("Gifts.csv")
 print(df)
+giftlist = ()
 
 while (1) :
         Decision = input("Add Gift(A) or Spin the wheel(S)  \n Choose A or S : ")
         flag = 0
         if Decision == "A" :
             gift = input("Enter Gift Name ")
-            amount = input("Enter the Number of Gift : ")
+            amount = int(input("Enter the Number of Gift : "))
             weight = input("Weight of the Gift : ")
             for i in range(len(df)) :
                 if df['Gift'][i] == gift :
-                    df.loc[i,'Amount'] = amount
+                    df.loc[i,'Amount'] -= amount
                     df.loc[i,'Weight'] = weight
                     flag=1
                 
 
             if flag==0 :
-                df.append([len(df)+1, gift,amount,weight]) 
+                df.loc[i+1,'Gift'] = gift
+                df.loc[i+1,'Amount'] = amount
+                df.loc[i+1,'Weight'] = weight
 
         elif Decision == "S" :
             rand = random.randint(0,100)
@@ -45,8 +49,25 @@ while (1) :
                 for col in df["Weight"]:
                         x = int(col)
                         prob = x/total
-                        print(prob)
-            break
+                        k.append(prob)
+
+                giftlist = list(df["Gift"])
+                giftDict = dict(zip(k,giftlist))
+                
+                #giftDict = dict(giftDict)
+                rand = random.randint(0,100)/100
+                for i in sorted(giftDict):
+                    if rand < i :
+                        print("Congratulations You have won ", giftDict[i])
+                        gift = giftDict[i]
+                        break
+                
+                for i in range(len(df)) :
+                    if df['Gift'][i] == gift :
+
+                         df.loc[i,'Amount'] -= 1
+
+            #break
 
         else :
             print("Input is Wrong ! Choose Between A / S ")
